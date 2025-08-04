f53298eabdf0c29bdbb1d484fba47d8278d58429
Vaibhav Dange <vaibhavdange23@gmail.com>
2025-08-04 15:22:34 +0530

    Added new features

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 function add(a, b) {
   return a + b;
 }
+function multiply(a, b) {
+  return a * b;
+}
+function subtract(a, b) {
+  return a - b;
+}
 
 module.exports = add;
 
 if (require.main === module) {
   console.log("Sum:", add(2, 3));
 }
+
